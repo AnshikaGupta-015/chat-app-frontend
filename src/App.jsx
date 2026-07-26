c5d6962b90a6bb3ea22235559bba16da44118eb0
AnshikaGupta-015 <anshikagupta0177@gmail.com>
2026-07-26 20:53:32 +0530

    "basic file created"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Route , Routes} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+
 
 const App = () => {
+
+   const authUser = false;
   return (
     <div className=''>
       <Routes>
          <Routes path = "/" element={authUser ? <HomePage /> : <Navigate to = "/login" />} />
          <Routes path = "/signup" element={authUser ? <SignUpPage /> : <Navigate to = "/" />}  />
          <Routes path = "/login" element={authUser ? <LoginPage /> : <Navigate to = "/" />}  />
-         <Routes path = "/settings" element={<SettingPage />}  />
+         <Routes path = "/settings" element={<SettingsPage />}  />
          <Routes path = "/profile" element={authUser ? <ProfilePage /> : <Navigate to = "/login" />} />
       </Routes>
     </div>
