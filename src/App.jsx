56f943158aa573d889abba791fc7ca058d4d0602
AnshikaGupta-015 <anshikagupta0177@gmail.com>
2026-07-26 21:52:08 +0530

    "updted file"

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import React from 'react'
-import { Route , Routes} from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import SettingsPage from './pages/SettingsPage';
-import ProfilePage from './pages/ProfilePage';
-
+import ProfilePage from './pages/ProfilePage'; 
 
 const App = () => {
-
-   const authUser = false;
+  const authUser = false;
   return (
     <div className=''>
       <Routes>
-         <Routes path = "/" element={authUser ? <HomePage /> : <Navigate to = "/login" />} />
-         <Routes path = "/signup" element={authUser ? <SignUpPage /> : <Navigate to = "/" />}  />
-         <Routes path = "/login" element={authUser ? <LoginPage /> : <Navigate to = "/" />}  />
-         <Routes path = "/settings" element={<SettingsPage />}  />
-         <Routes path = "/profile" element={authUser ? <ProfilePage /> : <Navigate to = "/login" />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
     </div>
   )
 }
 
 export default App
-
