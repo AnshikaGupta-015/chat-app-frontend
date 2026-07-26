fe3f67e0ee61a44290377733d32bedf7370956c1
AnshikaGupta-015 <anshikagupta0177@gmail.com>
2026-07-26 20:47:20 +0530

    "basic initials"

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div className=''>
-      APP
+      <Routes>
+         <Routes path = "/" element={authUser ? <HomePage /> : <Navigate to = "/login" />} />
+         <Routes path = "/signup" element={authUser ? <SignUpPage /> : <Navigate to = "/" />}  />
+         <Routes path = "/login" element={authUser ? <LoginPage /> : <Navigate to = "/" />}  />
+         <Routes path = "/settings" element={<SettingPage />}  />
+         <Routes path = "/profile" element={authUser ? <ProfilePage /> : <Navigate to = "/login" />} />
+      </Routes>
     </div>
   )
 }
