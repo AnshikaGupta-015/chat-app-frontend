e18ada4cb1f9366590b40eaab462929179742642
AnshikaGupta-015 <anshikagupta0177@gmail.com>
2026-07-31 11:31:14 +0530

    "authstore.js"

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import { create } from "Zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 // import { io } from "socket.io-client";
